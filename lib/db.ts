a7848336e4c28597f4b3f5efb2af4eee80ed5451
dlwhitmer <dlwhitmer@live.com>
2026-05-16 12:13:39 -0400

    Updated to use Turso DB

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,18 @@
 import { createClient } from "@libsql/client";
 
-export const db = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN!,
-});
+let db: ReturnType<typeof createClient> | null = null;
+
+export function getDb() {
+  if (!db) {
+    if (!process.env.TURSO_DATABASE_URL || !process.env.TURSO_AUTH_TOKEN) {
+      throw new Error("Missing Turso environment variables");
+    }
+
+    db = createClient({
+      url: process.env.TURSO_DATABASE_URL,
+      authToken: process.env.TURSO_AUTH_TOKEN,
+    });
+  }
+
+  return db;
+}
