aeaeae36def67a69b6c49f83f715cd5b4130ea51
dlwhitmer <dlwhitmer@live.com>
2026-07-16 15:56:31 -0400

    added filters to all cards

--- app/api/magic/packs/add/route.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "../../../../../lib/db/db";
-import { magicPacks } from "../../../../../lib/db/schema";
-
-export async function POST(req: Request) {
-  const body = await req.json();
-
-  const inserted = await db.insert(magicPacks).values(body).returning();
-  return Response.json({ data: inserted[0] });
-}
--- app/api/pokemon/packs/add/route.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "../../../../../lib/db/db";
-import { pokemonPacks } from "../../../../../lib/db/schema";
-
-export async function POST(req: Request) {
-  const body = await req.json();
-
-  const inserted = await db.insert(pokemonPacks).values(body).returning();
-  return Response.json({ data: inserted[0] });
-}
