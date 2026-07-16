ebb9c5642578035fff4b9681157651f87a2eb813
dlwhitmer <dlwhitmer@live.com>
2026-07-16 16:00:11 -0400

    added filters to all cards

--- app/api/magic/sets/add/route.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "../../../../../lib/db/db";
-import { magicSets } from "../../../../../lib/db/schema";
-
-export async function POST(req: Request) {
-  const body = await req.json();
-
-  const inserted = await db.insert(magicSets).values(body).returning();
-  return Response.json({ data: inserted[0] });
-}
